f7071cb6d4ff8a59ab0919bf575c16ea42d02f06
ROR0 <roshanjr3@gmail.com>
2021-09-16 09:49:45 -0400

    Mon premier commit

---
+++ textes/en/i18n.txt.php
@@ -0,0 +1,56 @@
+<?php
+
+    // changer le titre des pages et la description
+    $meta = [
+        "accueil" => [
+                        "titre" => "Welcome", 
+                        "desc"  => "Restaurant Leila - Montréal",
+                        "h1"    => "Leila"
+                    ],
+
+        "menu"    => [
+                        "titre" => "Our Menus", 
+                        "desc"  => "Menu du restaurant Leila à Montréal.",
+                        "h1"    => "Menu"
+                    ],
+
+        "vins"    => [
+                        "titre" => "wine Menu", 
+                        "desc"  => "Carte des vins du restaurant Leila à Montréal",
+                        "h1"    => "Wine list"
+                    ]
+    ];
+
+    //texte statique du restaurant Leila.
+    
+    //header (entête)
+    $en_sousTitre = "BISTRONOMIC CUISINE";
+    $en_heuresOuverture = "Opened today until 23:00";
+    $en_adresse = "275 street Notre-Dame East, Montreal, Quebec";
+    $en_telephone = "(514) 958-2580";
+
+    
+    //entete navigation principale
+
+    $en_altMenu = "The menus";
+    $en_altVins = "Wine menus";
+    $en_altHoraire = "Réservation en ligne - à venir";
+
+
+    //pied de page
+
+    $pp_infoTitre = "useful information";
+    $pp_heuresOuverture1 = "Cuisine ouverte de 11 h à 22 h.";
+    $pp_heuresOuverture2 = "Fermé le lundi.";
+    $pp_etiquetteReservation = "Pour réservation : ";
+    $pp_telephone = "(514) 958-2580";
+    $pp_titreGoogleMap = "Cliquez ici pour localiser le restaurant sur Google Maps";
+
+
+    //page d'accueil
+
+    $ac_titrePage = "Cuisine créative de saison";
+    
+
+
+?>
